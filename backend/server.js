0082bc63b6be5f6196cf3fc1486aa21ab10dc849
GarvPatidar <patidargarv45@gmail.com>
2026-06-17 15:15:20 +0530

    Fix CORS configuration in server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {[], methods: ["GET", "POST"] },
+  cors: { methods: ["GET", "POST"] },
 });
 
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
